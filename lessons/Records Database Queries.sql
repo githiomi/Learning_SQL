f10a647f8499a7bc08cde944bd095463ed27f066
Daniel Githiomi <danielgithiomi@gmail.com>
2023-04-16 11:22:49 +0400

    Created database and all required tables in SQL query

--- lessons/Records Database Queries.sql
+++ lessons/Records Database Queries.sql
@@ -1,1 +1,20 @@
-CREATE DATABASE records_database;
+CREATE DATABASE records_company_database;
+
+-- Specify the DB to be used in queries --
+USE records_company_database;
+
+-- Create the required tables --
+CREATE TABLE bands (
+	id INT NOT NULL AUTO_INCREMENT,
+    band_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE albums (
+	id INT AUTO_INCREMENT NOT NULL,
+    album_name VARCHAR(255) NOT NULL,
+    release_year INT,
+    band_id INT NOT NULL,
+    PRIMARY KEY (id),
+	CONSTRAINT FK_BandID FOREIGN KEY (band_id) REFERENCES bands(id)
+);
